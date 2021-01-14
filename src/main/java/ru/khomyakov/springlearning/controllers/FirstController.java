df92e64d6b57ce746d770ba304f7271c9bd24be1
Homval <in00@list.ru>
2021-01-14 21:00:15 +0300

    @RequestParam commit

--- src/main/java/ru/khomyakov/springlearning/controllers/FirstController.java
+++ src/main/java/ru/khomyakov/springlearning/controllers/FirstController.java
@@ -2,12 +2,16 @@ package ru.khomyakov.springlearning.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class FirstController {
 
     @GetMapping("/hello")
-    public String onPageHello() {
+    public String onPageHello(@RequestParam(value = "name", required = false) String name,
+                              @RequestParam(value = "surname", required = false) String surname) {
+
+        System.out.println("Hello " + name + " " + surname);
         return "/first/hello";
     }
 
